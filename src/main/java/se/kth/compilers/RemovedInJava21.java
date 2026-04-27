754eb9a2a10df88585c17bd042b4bb133fda2ee3
pjacewic <piotr.jacewicz@capgemini.com>
2026-04-27 14:53:07 +0200

    Updated Java to 21

--- src/main/java/se/kth/compilers/RemovedInJava21.java
+++ src/main/java/se/kth/compilers/RemovedInJava21.java
@@ -2,18 +2,31 @@ package se.kth.compilers;
 
 public class RemovedInJava21 {
     public void runAll() {
-        compiler();
+        // Compiler.ENABLE is removed in Java 21, no direct replacement
+        // TODO: This functionality is removed in Java 21. No direct replacement available.
+        // System.out.println("Compiler.ENABLE is removed in Java 21.");
+
+        // allowThreadSuspension is removed in Java 21, replaced with Thread.onSpinWait()
+        // TODO: This functionality is replaced with Thread.onSpinWait() in Java 21.
         allowThreadSuspension();
     }
 
+    // Removed in Java 21, no direct replacement
+    // TODO: This functionality is removed in Java 21. No direct replacement available.
     void compiler() {
-        Compiler.enable();
-        System.out.println("Compiler.enable() works.");
+        // Compiler.ENABLE is removed in Java 21
+        // TODO: This functionality is removed in Java 21. No direct replacement available.
+        System.out.println("Compiler.ENABLE is removed in Java 21.");
     }
 
+    // Replaced with Thread.onSpinWait() in Java 21
+    // TODO: This functionality is replaced with Thread.onSpinWait() in Java 21.
     void allowThreadSuspension() {
-        ThreadGroup group = Thread.currentThread().getThreadGroup();
-        boolean ok = group.allowThreadSuspension(true);
-        System.out.println("ThreadGroup.allowThreadSuspension(boolean) works - result: " + ok);
+        // Thread.onSpinWait() is the replacement for ThreadGroup.allowThreadSuspension() in Java 21
+        // TODO: This functionality is replaced with Thread.onSpinWait() in Java 21.
+        for (int i = 0; i < 1000; i++) {
+            Thread.onSpinWait();
+        }
+        System.out.println("Thread.onSpinWait() works - result: true");
     }
 }
